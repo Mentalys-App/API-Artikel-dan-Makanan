68ac41794adeed4880af2a845467bd617906a552
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-04 09:05:22 +0700

    create a new route for '/', and implemented the notFound middleware and globalErrorHandler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import express, { type Application } from 'express'
+import express, { type Application, type Request, type Response } from 'express'
 import 'dotenv/config'
+import { notFound } from './controllers/notFoundController'
+import { globalErrorHandler } from './controllers/errorController'
 
 const app: Application = express()
 const port: number = Number(process.env.PORT) || 3000
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.all('*', notFound)
+app.use(globalErrorHandler)
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port http://localhost:${port}`)
 })
