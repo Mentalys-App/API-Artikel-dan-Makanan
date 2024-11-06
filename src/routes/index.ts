ffca415bfb8b90a372decabcb9c7e2ff6d193725
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-06 13:15:17 +0700

    Install Dependency swagger and Create swagger configuration for API Documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router } from 'express'
 import articleRouter from './articleRouter'
 import { notFound } from '../middleware/error/notFoundController'
 import { globalErrorHandler } from '../middleware/error/errorController'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
+import path from 'path'
 
 const app = Router()
+// Load YAML file
+const swaggerDocument = YAML.load(path.join(__dirname, '../../swagger.yaml'))
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/api/v1', articleRouter)
 app.use('*', notFound)
 app.use(globalErrorHandler)
