ab1c761649cb8e80d68c31e9a003f6f94b85fdf8
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-30 14:47:16 +0700

    refactor: remove default port fallback for cleaner configuration
    - Removed the fallback value for `PORT` to ensure the application strictly relies on environment variable configuration.
    
    This change enforces consistency in deployment environments and prevents unintended behavior due to missing or incorrect `PORT` values.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import web from './middleware/web'
 
-const port: number = Number(process.env.PORT) || 3000
+const port: number = Number(process.env.PORT)
 web.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
 })
